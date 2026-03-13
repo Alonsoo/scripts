14c9531012976030220e734a7e6ab7c59c1feb75
Alonso <alonso@losmolina.org>
2026-03-12 19:40:17 -0600

    Added audio sink icon support in volume status script

--- statusbar/volume.sh
+++ statusbar/volume.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
 VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut --characters 9- | sed "s/\.//" )
-# LEVEL=$(((${VOLUME#0} + 5) / 10)) # ${VOLUME#0} removes leading zero which would cause bash to interpret number as octal
 
-TENS=$((${VOLUME#0} / 10))
+TENS=$((${VOLUME#0} / 10)) # ${VOLUME#0} removes leading zero which would cause bash to interpret number as octal
 UNITS=$((${VOLUME#0} % 10))
 
 LINE="─"
@@ -14,8 +13,8 @@ INDICATOR_RIGHT="╼"
 OUTPUT=""
 
 case $UNITS in
-	0) INDICATOR=$INDICATOR_LEFT ;;
-	5) INDICATOR=$INDICATOR_RIGHT ;;
+	[0-4]) INDICATOR=$INDICATOR_LEFT ;;
+	[5-9]) INDICATOR=$INDICATOR_RIGHT ;;
 	+) INDICATOR="?" ;;
 esac
 
@@ -26,22 +25,29 @@ for i in {0..10}; do
 	esac
 done
 
-#for i in {0..10}; do
-#	#if [[ $i == $LEVEL ]]
-#	case $(i) in
-#		$LEVEL)
-#			case $i in
-#				0) INDICATOR=$INDICATOR_LEFT ;;
-#				10) INDICATOR=$INDICATOR_RIGHT ;;
-#				*) INDICATOR=$INDICATOR_CENTER ;;
-#			esac
-#			OUTPUT="$OUTPUT$INDICATOR"
-#			;;
-#		*) OUTPUT="$OUTPUT$LINE" ;;
-#	esac
-#done
+headphones_plugged() {
+	amixer -c 0 cget iface=CARD,name='Headphone Jack' 2>/dev/null | grep -q "values=on"
+}
+bluetooth_connected() {
+	pactl info | grep "Default Sink" | grep -q "bluez"
+}
+usb_audio() {
+	pactl info | grep "Default Sink" | grep -q "usb"
+}
 
-echo "󰕾 $OUTPUT ${VOLUME#0}"
+ICON="?"
+if bluetooth_connected; then
+	ICON=""
+elif usb_audio; then
+	ICON="󰗅"
+elif headphones_plugged; then
+	ICON="󰋋"
+else
+	ICON="󰕾"
+fi
+
+
+echo "$ICON $OUTPUT ${VOLUME#0}"
 
 # ────────── 2┤├───  ─╼  ╾
 
