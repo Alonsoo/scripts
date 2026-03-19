ca0ffe1b35052b02e2c900d6f098164a556b417b
Alonso <alonso@losmolina.org>
2026-03-19 15:38:53 -0600

    Added wmenu-colors for pywal16 color theme setting and replaced wmenu with wmenu-colors i various scripts

--- power-options.sh
+++ power-options.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-case "$(printf "kill\nlogout\nzzz\nreboot\nshutdown" | wmenu -i -c -l 5)" in
-	kill) ps -u "$USER" -o pid,comm,%cpu,%mem | wmenu -i -c -l 10 -p Kill: | awk '{print $1}' | xargs -r kill ;;
+case "$(printf "kill\nlogout\nzzz\nreboot\nshutdown" | wmenu-colors -i -c -l 5)" in
+	kill) ps -u "$USER" -o pid,comm,%cpu,%mem | wmenu-colors -i -c -l 10 -p Kill: | awk '{print $1}' | xargs -r kill ;;
 	logout) echo TODO;;
 	zzz) systemctl suspend ;;
 	reboot) systemctl reboot -i ;;
---
+++ wmenu-colors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+. "${HOME}/.cache/wal/colors.sh"
+wmenu -S "$color1" -M "$color1" "$@"
